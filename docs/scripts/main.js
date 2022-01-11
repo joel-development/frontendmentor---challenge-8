7894b402eaaa37e343c8413c8bcb3c846ec8fc6b
Joel Sanchez <joelalejandro.development@gmail.com>
2022-01-11 19:56:30 -0400

    attribution added, background-menu scroll

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -5,3 +5,18 @@ toggle.addEventListener('click', ()=>
 {
   menu.classList.toggle('menu--on')
 })
+window.addEventListener('scroll',()=>
+{
+  if(menu.classList.contains('menu--on'))
+  {
+    menu.classList.remove('menu--on')
+  }
+  if(window.scrollY > window.innerHeight - 100)
+  {
+    header.classList.add('header--scroll-bg')
+  }
+  else
+  {
+    header.classList.remove('header--scroll-bg')
+  }
+})
